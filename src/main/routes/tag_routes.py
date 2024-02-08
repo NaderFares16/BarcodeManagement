6fb308b38c81675c6b9b8e4afc90c8885fa59d4f
Nader Fares <naderalifares16@hotmail.com>
2024-02-07 23:35:18 -0300

    Implementing Cerberus Validator

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from src.views.http_types.http_resquest import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
@@ -9,6 +10,7 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 def create_tags():
   response = None
   try:
+    tag_creator_validator(request)
     tag_creator_view = TagCreatorView()
 
     http_resquest = HttpRequest(body=request.json)
